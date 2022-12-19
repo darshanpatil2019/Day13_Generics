6ceab0a1d5ada570824b9422a4e94626c4b16956
Darshan Patil <iampatildarsh>
2022-12-19 22:36:43 +0530

    Generic Class for Find Maximum Values

--- src/FindMaxValue.java
+++ src/FindMaxValue.java
@@ -1,7 +1,28 @@
-public class FindMaxValue {
+public class FindMaxValue<T extends Comparable<T>> {
+
+    T a, b, c;
+
+    //Generic Constructor for Handling Different Type Object
+    FindMaxValue(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public static void main(String[] args) {
+        //Creating Generic Integer Object for Integer Comparison
+        FindMaxValue<Integer> integerComparison = new FindMaxValue(40, 60, 100);
+        System.out.println("Maximum from 3 Integer Object : " + integerComparison.compareTo());
+        //Creating Generic Float Object for Float Comparison
+        FindMaxValue<Float> floatComparison = new FindMaxValue<>(40.00F, 60.00F, 100.00F);
+        System.out.println("Maximum from 3 Float Object : " + floatComparison.compareTo());
+        //Creating Generic String Object for String Comparison
+        FindMaxValue<String> stringComparison = new FindMaxValue<>("Apple", "Peach", "Banana");
+        System.out.println("Maximum from 3 String Object : " + stringComparison.compareTo());
+    }
 
     //compareTo method for Checking Maximum from 3 Integer Object
-    public <T extends Comparable<T>> T compareTo(T a, T b, T c) {
+    public T compareTo() {
         if (a.compareTo(b) > 0) {
             if (a.compareTo(c) > 0) {
                 return a;
@@ -15,17 +36,5 @@ public class FindMaxValue {
                 return c;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        //Creating Generic Integer Object for Integer Comparison
-        FindMaxValue integerComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 Integer Object : " + integerComparison.compareTo(40, 60, 100));
-        //Creating Generic Float Object for Float Comparison
-        FindMaxValue floatComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 Float Object : " + floatComparison.compareTo(40.00F, 60.00F, 100.00F));
-        //Creating Generic String Object for String Comparison
-        FindMaxValue stringComparison = new FindMaxValue();
-        System.out.println("Maximum from 3 String Object : " + stringComparison.compareTo("Apple", "Peach", "Banana"));
     }
 }
