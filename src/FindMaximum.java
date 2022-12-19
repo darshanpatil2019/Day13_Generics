ca246bdd2ed5b750876e1749adcc0d55f6ad1aae
Darshan Patil <iampatildarsh>
2022-12-19 22:28:00 +0530

    Find Maximum of String Values

--- src/FindMaximum.java
+++ src/FindMaximum.java
@@ -39,5 +39,6 @@ public class FindMaximum {
     public static void main(String[] args) {
         System.out.println("Maximum of 3 integers is : " + maxInteger(40, 60, 100));
         System.out.println("Maximum of 3 floats is : " + maxFloat(40.00f,60.00f,100.00f));
+        System.out.println("Maximum of 3 Strings is : " + maxString("Apple","Peach","Banana" ));
     }
 }
